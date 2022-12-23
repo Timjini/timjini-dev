a7b5a66b39823c89029dceb1ee20fa89a87be1ae
timjini <hatim.jini@gmail.com>
2022-12-23 23:31:30 +0300

    blog style

--- pages/projects/[slug].jsx
+++ pages/projects/[slug].jsx
@@ -3,7 +3,7 @@ import Navbar from '../../components/Navbar';
 import Image from 'next/image';
 import Link from 'next/link';
 import react from '../../public/react-js-icon.svg';
-import styles from '../../styles/Home.module.css';
+import styles from '../../styles/Slug.module.css';
 
 const graphcsm = new GraphQLClient('https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clbukk0lj2sh801uk6syyh0f9/master');
 
@@ -59,10 +59,12 @@ export default function ProjectPage({ project }) {
     return (
         <><Navbar />
         <section className="">
-         <div className='flex justify-center items-center h-full flex-wrap bg-gray-100 p-24'>
-         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 text-gray-700">
+         <div className='flex justify-center items-center h-full flex-wrap bg-gray-100 p-12'>
+            <Link href='/' className='hover:text-green-300'>
+         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 text-gray-700 hover:text-green-400 mr-8">
             <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
+        </Link>
 
             <div className='mx-auto  text-center lg:mx-0 '>
                 <h1 className='text-3xl font-bold sm:text-4xl text-gray-700'>{project.title}</h1>
