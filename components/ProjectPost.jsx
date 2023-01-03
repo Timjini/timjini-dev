85d86395451d0a315674837215d7057b771e9cec
timjini <hatim.jini@gmail.com>
2023-01-03 17:04:27 +0300

    removal of stack pictures for project buttons

--- components/ProjectPost.jsx
+++ components/ProjectPost.jsx
@@ -6,38 +6,21 @@ import Link from 'next/link'
 
 function ProjectPost({title, excerpt, image, slug , stack,livesite, github}) {
   return (
-    <section className='flex justify-center items-center h-full flex-wrap gap-4' id="projects" key={title}>
-    <div className="max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-    <div className='mx-auto max-w-lg text-center lg:mx-0 lg:text-left'>
-    <h2 className="text-3xl font-bold sm:text-4xl">{title}</h2>
-    <p className="mt-4 mb-1 text-gray-600 box"> {excerpt}</p>
-      </div>
 
-
-      <div
-        className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16"
-      >
-        <div className="grid grid-cols-3 gap-4 sm:grid-cols-3  py-5" >
-          {stack.map((stack) => (
-         
-            // eslint-disable-next-line react/jsx-key
-            <Image src={stack.url} alt="" width={90} height={90}
-             className='hover:scale-110 transform transition duration-500 ease-in-out'
-             key={stack.url}
-            />
-
-          ))}
-          <Link href={"https://github.com/Timjini/"+slug} rel='noreferrer' target='_blank' className='m-2 text-gray-700 transition hover:text-gray-700/75'>github Repo</Link>
-          <Link href={`${livesite}`} target="_blank" className='m-2 text-gray-700 transition hover:text-gray-700/75'>Live Project</Link>
-          <Link href={"/projects/"+slug}  className='m-2 text-gray-700 transition hover:text-gray-700/75'>Read More</Link>
+    <section className='flex-post'>
+      <div className='flex-post-content bg-green-200'>
+        <h2 className="text-3xl font-bold sm:text-4xl">{title}</h2>
+        <p className="mt-4 mb-1 text-gray-600 box"> {excerpt}</p>
+        <div className='flex-post-links'>
+          <Link href={"https://github.com/Timjini/"+slug} rel='noreferrer' target='_blank' className='m-2 link-btn text-gray-700 transition hover:text-green-200/75'><span>github Repo</span></Link>
+          <Link href={`${livesite}`} target="_blank" className='m-2 text-gray-700 transition hover:text-gray-700/75 link-btn'><span>Live App</span></Link>
+          <Link href={"/projects/"+slug}  className='m-2 text-gray-700 transition hover:text-gray-700/75 link-btn'><span>Read more</span></Link>
         </div>
-        <div className="mx-auto max-w-lg text-center lg:mx-0 lg:text-left">
-          <Image src={image.url} alt="" width={500} height={500} />
-
+        </div> 
+        <div className='flex-post-img'>
+        <Image src={image.url} alt="" width={500} height={500} />
         </div>
-      </div>
-    </div>
-  </section>
+    </section>
   
   )
 }
